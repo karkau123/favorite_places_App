051b141f07b50f0804fbb94bfe8180baddf1e913
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-18 02:26:36 +0530

    Basic Layout Created

---
+++ lib/models/place.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/data.dart';
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+class Place {
+     Place({required this.title}): id=uuid.v4();
+
+  final String id;
+  final String title;
+}
---
+++ lib/screens/add_place.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+           TextField(decoration: InputDecoration(labelText: 'title'),)
+        ],
+      )),
+    );
+  }
+}
