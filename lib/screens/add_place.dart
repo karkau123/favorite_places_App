63171145ed33455bb9eacd3a064e27e1577b7a38
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-18 02:56:48 +0530

    Navigating to the new Screen

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -8,18 +8,35 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
+   void dispose() {
+     _titleController.dispose();
+    super.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add new Place'),
       ),
       body: SingleChildScrollView(
+        padding:const EdgeInsets.all(12),
           child: Column(
+            
         children: [
-           TextField(decoration: InputDecoration(labelText: 'title'),)
+           TextField(decoration:const InputDecoration(labelText: 'title'),
+           controller: _titleController ,
+           ),
+          const  SizedBox(height: 16,),
+
+           ElevatedButton.icon(
+            onPressed:(){}, 
+            icon: Icon(Icons.add),
+            label: const Text('Add Place'))
         ],
-      )),
+      ),
+      ),
     );
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +8,19 @@ class PlacesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Places'),
-        actions: [
-          IconButton( icon: Icon(Icons.add),
-          onPressed: (){
-             
-          })
-        ],
-      ),
-
-      body: PlacesList(places: [],)
-    );
+        appBar: AppBar(
+          title: const Text('Your Places'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (ctx) => const AddPlaceScreen()));
+                })
+          ],
+        ),
+        body: PlacesList(
+          places: [],
+        ));
   }
 }
