2482c340672830f2df348f1678d1641ede87eed6
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-18 15:59:40 +0530

    Place Details Screen Added

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,24 +1,39 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_details.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
 
-  final List<Place>places; // list of place , where place consists of id and password.
-  
+  final List<Place>
+      places; // list of place , where place consists of id and password.
+
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return  Center(
-        child:  Text('No Places Added yet',style: Theme.of(context).textTheme.bodyLarge!.copyWith
-        (color: Theme.of(context).colorScheme.onBackground),),);
-    } 
-    
-      return   ListView.builder(
+      return Center(
+        child: Text(
+          'No Places Added yet',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      );
+    }
+
+    return ListView.builder(
         itemCount: places.length,
-        itemBuilder: (ctx , index)=>ListTile(
-        title:  Text(places[index].title,style: Theme.of(context).textTheme.titleMedium!.copyWith
-        (color: Theme.of(context).colorScheme.onBackground),),
-       ));
+        itemBuilder: (ctx, index) => ListTile(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (ctx) => PlaceDetailScreen(place: places[index])));
+              },
+              title: Text(
+                places[index].title,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ));
   }
 }
