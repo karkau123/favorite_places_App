b4f3d90508896e025f33ada51c8801fb1874da96
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-18 21:50:35 +0530

    Image picker added ,

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -43,6 +44,12 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               style:
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
+  
+  const SizedBox(
+              height: 10,
+            ),
+       Imageinput(),
+
             const SizedBox(
               height: 16,
             ),
--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -17,3 +17,4 @@ class PlaceDetailScreen extends StatelessWidget {
     );
   }
 }
+ 
