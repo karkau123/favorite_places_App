d1dfd98d1be22348d4676a765df611608fc19ea0
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-19 23:19:26 +0530

    new screen added (mapscree )

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/map.dart';
 import 'package:flutter/material.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
@@ -7,7 +8,6 @@ class PlaceDetailScreen extends StatelessWidget {
   final Place place;
 
   String get locationImage {
-  
     final lat = place.location.latitude;
     final lng = place.location.longitude;
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4ag';
@@ -28,40 +28,50 @@ class PlaceDetailScreen extends StatelessWidget {
             height: double.infinity,
           ),
           Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Column(
-               children: [
-                 CircleAvatar(
-                  radius:  70,
-                  backgroundImage: NetworkImage(locationImage),
-                 ),
-                 Container(
-                  alignment: Alignment.center,
-                  decoration:const  BoxDecoration(
-                      gradient: LinearGradient(colors:[
-                       Colors.transparent,
-                       Colors.black,
-                     ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter
-                     )
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (ctx) => MapScreen(
+                                 location: place.location,
+                                 isSelecting: false,
+
+                          ),
+                          ),
+                          );
+                    },
+                    child: CircleAvatar(
+                      radius: 70,
+                      backgroundImage: NetworkImage(locationImage),
+                    ),
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 16,
-                  ),
-                  child: Text(place.location.address, style: Theme.of(context)
-              .textTheme
-              .titleLarge!
-              .copyWith(color: Theme.of(context).colorScheme.onBackground),),
-                 )
-               ],
-            )
-            )
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          Colors.transparent,
+                          Colors.black,
+                        ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    child: Text(
+                      place.location.address,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  )
+                ],
+              ))
         ],
-        
       ),
     );
   }
